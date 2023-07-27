baf7196dbf2080e94499577e645bd6ee8f48d811
samuellvn <138622449+samuellvn@users.noreply.github.com>
2023-07-27 13:03:53 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var canvas;
+function preload(){
+
+}
+function setup(){
+  canvas=createCanvas(300, 300);
+  canvas.center();
+}
+function draw(){
+
+}
+function takeSnapshot(){
+  save("foto.png");
+}
